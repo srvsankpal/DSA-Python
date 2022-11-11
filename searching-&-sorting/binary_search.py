5ac1b47b808b9eb8627f5fc10e7ea07da6e6b8f1
Sourav Sankpal <72756866+srvsankpal@users.noreply.github.com>
2022-11-11 13:08:50 -0500

    Update binary_search.py
    added recursive method

--- searching-&-sorting/binary_search.py
+++ searching-&-sorting/binary_search.py
@@ -1,6 +1,5 @@
 #__Iterative Method__#
-
-def binary_search(input_array, value):
+def binary_search_iterative(input_array, value):
     low=0
     high=len(input_array)-1
     while True:
@@ -13,3 +12,19 @@ def binary_search(input_array, value):
             low=mid+1
         elif value<input_array[mid]:
             high=mid-1
+
+ #__Recursive Method__#
+def binary_search(input_array, value, low=0, high=None):
+    if high==None:
+        high=len(input_array)-1
+    mid=(low+high)//2
+    if value==input_array[mid]:
+        return mid
+    elif low==high:
+        return -1
+    elif value>input_array[mid]:
+        low=mid+1
+        return binary_search(input_array, value, low, high)
+    elif value<input_array[mid]:
+        high=mid-1
+        return binary_search(input_array, value, low, high)
